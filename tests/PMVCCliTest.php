872db8212c25d9a2c0eb1d96d389d141d4038257
Hill <hill@kimo.com>
2023-09-09 14:30:43 +0800

    update phpunit10 cli

--- tests/PMVCCliTest.php
+++ tests/PMVCCliTest.php
@@ -12,6 +12,7 @@ class PMVCCliTest extends TestCase
     function testBin()
     {
         exec('php ' . __DIR__ . '/../bin/pmvc 2>/dev/null', $output);
+        // fwrite(STDERR, print_r($output, true));
         $this->haveString('Workplace', $output[0]);
     }
 }
